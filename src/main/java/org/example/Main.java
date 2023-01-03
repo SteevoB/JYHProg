1ca9f1a1c6dc4b6e187c56d03fb1b38cea0905ab
Steven B <steven.bergqvist@hotmail.com>
2023-01-04 00:03:29 +0100

    1st commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+class Calculator implements ActionListener{
+
+    JFrame frame;
+    JTextField;
+    JButton[] numBtns = new JButton[10];
+    JButton[] funcBtns = new JButton[8];
+    JButton subBtn, divBtn, multBtn, addBtn, commaBtn, equBtn, backBtn, clrBtn;
+}
